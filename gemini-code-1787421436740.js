ce7c960efd59eedd71678a8b1e82fd06f2075a8a
tamelessbot-beep <tamelessbot@gmail.com>
2026-08-22 12:57:48 -0500

    Add files via upload

---
+++ gemini-code-1787421436740.js
@@ -0,0 +1,5 @@
+const orderButton = document.getElementById("orderBtn");
+
+orderButton.addEventListener("click", function() {
+    alert("Thanks for choosing Scooter's Coffee! Your order has been placed.");
+});
